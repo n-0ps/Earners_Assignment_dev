5f038559eddfe8371498a85cd9918c6988b67b16
N7 <nee7aj@hotmail.com>
2026-08-16 22:53:04 +0530

    done lec 30

--- JavaScript/Lecture 30-Operators, Conditionals, Loops/index.js
+++ JavaScript/Lecture 30-Operators, Conditionals, Loops/index.js
@@ -48,7 +48,7 @@ num /= 5;
 console.log(num); // 2
 num %= 5;
 console.log(num); // 2
-num**=5;
+num **= 5;
 console.log(num); // 32
 
 
@@ -82,14 +82,14 @@ console.log(true || false);
 const age = 17;
 const hasId = true;
 
-const canEnterClub = (age>= 18 && hasId == true);
+const canEnterClub = (age >= 18 && hasId == true);
 console.log(canEnterClub);
 
 console.log(!true);  // opposite kar deta hai
 console.log(!56); // output: false
 console.log(!0); // output: true
 
-console.log("manish"/60); // output: NaN = Not a Number
+console.log("manish" / 60); // output: NaN = Not a Number
 console.log(typeof NaN);
 
 
@@ -102,7 +102,7 @@ const isLoggedIn = false;
 // if(2-2){ //false
 //     console.log("You can like, comment");
 // }
-if(isLoggedIn){
+if (isLoggedIn) {
     console.log("You can like, comment");
 }
 else {
@@ -115,7 +115,7 @@ else {
 
 let temp = 40;
 
-if(temp>= 25){
+if (temp >= 25) {
     console.log("AC chala do");
 } else {
     console.log("AC mat chalao")
@@ -124,27 +124,98 @@ if(temp>= 25){
 
 let day = "monday";
 
-if (day === "monday"){
+if (day === "monday") {
     console.log("1st day of the week");
 }
-else if (day === "tuesday"){
+else if (day === "tuesday") {
     console.log("2nd day of the week");
 }
-else if (day === "wednesday"){
+else if (day === "wednesday") {
     console.log("3rd day of the week");
 }
-else if (day === "thursday"){
+else if (day === "thursday") {
     console.log("4th day of the week");
 }
-else if (day === "friday"){
+else if (day === "friday") {
     console.log("5th day of the week");
 }
-else if (day === "saturday"){
+else if (day === "saturday") {
     console.log("6th day of the week");
 }
-else if (day === "sunday"){
+else if (day === "sunday") {
     console.log("7th day of the week");
 }
 else {
     console.log("Wrong day");
+}
+
+
+
+
+
+
+// score to grade convert
+
+
+
+
+
+
+
+
+
+
+
+
+// nestedif else jio hotstar
+
+
+const isLoggedIn = true;
+const isSubscribed = false;
+
+if (isLoggedIn) {
+    if (isSubscribed) {
+        console.log{
+            "You can access premium content"
+        }
+    }
+    else {
+        console.log("You don't have any premium plan to access this content")
+    }
+}
+else {
+    console.log("Please login")
+}
+
+
+
+
+// switch case
+
+const day = "fri";
+
+switch (day) {
+    case "non";
+        console.log("1st day of the week")
+        break;
+    case "tue";
+        console.log("2nd day of the week")
+        break;
+    case "wed";
+        console.log("3rd day of the week")
+        break;
+    case "thur";
+        console.log("4th day of the week")
+        break;
+    case "fri";
+        console.log("5th day of the week")
+        break;
+    case "sat";
+        console.log("6th day of the week")
+        break;
+    case "sun";
+        console.log("7th day of the week")
+        break;
+    default :6
+        console.log("wrong day");
 }
